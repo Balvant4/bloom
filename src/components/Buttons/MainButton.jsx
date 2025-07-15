9f9d7e15bc8ab937bfce926a22cb0f5348e19d25
Balvant4 <balvantkushwaha44@gmail.com>
2025-07-16 00:00:20 +0530

    Update MainButton styles for improved visibility and consistency

--- src/components/Buttons/MainButton.jsx
+++ src/components/Buttons/MainButton.jsx
@@ -5,7 +5,7 @@ function MainButton({ text = "Login or Join", href = "/" }) {
   return (
     <Link
       href={href}
-      className="hover:text-yellow-300 transition px-3 cursor-pointer py-2 font-bold border-0 xl:border-2 rounded-md inline-block text-white"
+      className="hover:text-yellow-300 transition px-3 cursor-pointer py-2 font-bold border xl:border-2 rounded-md inline-block xl:text-white text-yellow-300 "
     >
       {text}
     </Link>
